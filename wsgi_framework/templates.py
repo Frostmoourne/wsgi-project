d4a400c627597f228a8e1ef6b9ed37396b748d22
Artem <shalyapin.artyom@yandex.ru>
2020-12-13 17:10:05 +0500

    ver0.3

--- wsgi_framework/templates.py
+++ wsgi_framework/templates.py
@@ -1,5 +1,5 @@
-from jinja2 import Template
-import os
+from jinja2 import Template, FileSystemLoader
+from jinja2.environment import Environment
 
 
 def render(template_name, folder='templates', **kwargs):
@@ -10,9 +10,10 @@ def render(template_name, folder='templates', **kwargs):
     :param kwargs:
     :return:
     """
-    path = os.path.join(folder, template_name)
 
-    with open(path, encoding='utf-8') as file:
-        template = Template(file.read())
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+
+    template = env.get_template(template_name)
 
     return template.render(**kwargs)
